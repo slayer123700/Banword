636699da38639af101eb513ff720a085c6c9850f
slayer <meerutuniversity996@gmail.com>
2026-01-30 13:11:21 +0530

    Update __init__.py

--- Banword/__init__.py
+++ Banword/__init__.py
@@ -1,39 +1,10 @@
-import asyncio
-import logging
-import time
-from importlib import import_module
-from os import listdir, path
-from dotenv import load_dotenv
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN, BOT_USERNAME, OWNER_ID
+from config import API_ID, API_HASH, BOT_TOKEN
 
-loop = asyncio.get_event_loop()
-load_dotenv()
-boot = time.time()
-
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.INFO,
-)
-
-# Define app here (was Banword before)
 app = Client(
-    ":Banword:",
+    "Banword",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
+    in_memory=True
 )
-
-async def start_bot():
-    global BOT_ID, BOT_NAME, BOT_USERNAME
-    await app.start()
-    getme = await app.get_me()
-    BOT_ID = getme.id
-    BOT_USERNAME = getme.username
-    if getme.last_name:
-        BOT_NAME = getme.first_name + " " + getme.last_name
-    else:
-        BOT_NAME = getme.first_name
-
-# Start the bot
-loop.run_until_complete(start_bot())
