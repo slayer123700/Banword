bf013b654ba1c64dad562eea06bcf4398fa4cb9e
slayer <meerutuniversity996@gmail.com>
2026-01-30 12:52:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,21 +8,21 @@ API_HASH = os.environ.get("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
 
 # ------------------------------------------------
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "VegitoEditGurdian_Bot")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8287492525:AAHgkD4zbbRk_4937966-WVObyrmIqVPnuw")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Rion_xbot")
 
 # -----------------------------------------------
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "8383747125"))
-SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "8195241636"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6018803920"))
+SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "6018803920))
 # ------------------------------------------------
 
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002417142493"))
-OTHER_LOGS = int(os.environ.get("OTHER_LOGS", "-1002417142493"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002745123292"))
+OTHER_LOGS = int(os.environ.get("OTHER_LOGS", "-1002745123292"))
 
 # ------------------------------------------------
 
-MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://lollolopp0900:slayersan@cluster0.mge1ngz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # ------------------------------------------------
